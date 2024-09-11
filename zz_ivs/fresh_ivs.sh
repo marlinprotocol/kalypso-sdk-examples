cb744bdd8cf02e187fb13a84175c5946f40df5db
Akshay Meher <akshay_111meher@hotmail.com>
2024-09-11 14:51:39 +0400

    update

--- zz_ivs/fresh_ivs.sh
+++
@@ -1,4 +0,0 @@
-yarn test ./examples/marketMakerOperations/0_generate_ivs_apikey.ts
-yarn test ./examples/marketMakerOperations/1_ivs_config.ts
-yarn test ./examples/marketMakerOperations/2_start_ivs.ts
-yarn test ./examples/marketMakerOperations/99_add_ivs_market.ts
